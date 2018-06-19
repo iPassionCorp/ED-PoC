d9f2820ed7e913aac92fe8bd81a7f43315e28718
M33POOH <natapon.p@ipassion.co.th>
2018-06-19 15:18:54 +0700

    config weblogic

--- ED-RestAPI/src/main/java/th/co/ipassion/ed/Application.java
+++ ED-RestAPI/src/main/java/th/co/ipassion/ed/Application.java
@@ -4,19 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer{
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
     
     @Bean
     public WebMvcConfigurer corsConfigurer() {
